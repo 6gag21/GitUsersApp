4262b91d54235869603a0c98f7f878063e10a1ee
6gag21 <38590564+6gag21@users.noreply.github.com>
2019-02-05 16:33:38 +0400

    1

--- app/src/main/java/com/example/aca/gitusersapp/adapter/CustomScrollListener.java
+++ app/src/main/java/com/example/aca/gitusersapp/adapter/CustomScrollListener.java
@@ -7,10 +7,8 @@ import android.support.v7.widget.RecyclerView;
 
 public abstract class CustomScrollListener extends RecyclerView.OnScrollListener {
 
-    private static int ITEMS_COUNT = 10;
     private boolean isLoading = false;
     private boolean isLastPage = false;
-    private int currentPage = 0;
 
     public void setLoading(boolean loading) {
         isLoading = loading;
@@ -26,26 +24,6 @@ public abstract class CustomScrollListener extends RecyclerView.OnScrollListener
         this.mLayoutManager = layoutManager;
     }
 
-//    @Override
-//    public void onScrolled(@NonNull RecyclerView view, int dx, int dy) {
-//        super.onScrolled(view, dx, dy);
-//        int lastVisibleItemPosition = 0;
-//        int totalItemCount = mLayoutManager.getItemCount();
-//
-//
-//        lastVisibleItemPosition = mLayoutManager.findLastVisibleItemPosition();
-//
-//        if (loading && (totalItemCount > previousTotalItemCount)) {
-//            loading = false;
-//            previousTotalItemCount = totalItemCount;
-//        }
-//
-//        if (!loading && (lastVisibleItemPosition + VISIBLE_TRESHHOLD) > totalItemCount) {
-//            currentPage++;
-//            onLoadMore(currentPage, totalItemCount, view);
-//            loading = true;
-//        }
-//    }
 
     @Override
     public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
@@ -61,7 +39,6 @@ public abstract class CustomScrollListener extends RecyclerView.OnScrollListener
 
         if (!isLoading && !isLastPage) {
             if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount) {
-                currentPage++;
                 onLoadMore(totalItemCount);
                 isLoading = true;
             }
